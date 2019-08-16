569f4d58f573b8779df6e45eebe63bfb8e0b6e0a
Henrique Geremia Nievinski <henriquegeremia@gmail.com>
2019-08-16 15:52:44 -0300

    Create run_multiple_qvws.bat

---
+++ run_multiple_qvws.bat
@@ -0,0 +1,11 @@
+// save the .bat file to UTF-8 charset
+chcp 65001
+
+
+// "C:\Program Files\QlikView\Qv.exe" : Default qlikview executable adress
+// /r : reload, save and close
+// /nodata : open file without data
+// "G:\Project Folder\App Name.qvw" : file address
+"C:\Program Files\QlikView\Qv.exe" /r /nodata "G:\Project Folder\App Name.qvw"
+
+"C:\Program Files\QlikView\Qv.exe" /r /nodata "G:\Project Folder\Another App Name.qvw"
